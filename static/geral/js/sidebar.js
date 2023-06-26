bf04892bc5ec48b015df8c439b79b9ae5d0dea58
Octavio-Lima <116118454+Octavio-Lima@users.noreply.github.com>
2023-06-26 20:42:08 -0300

    Adicionado Barra Lateral Dinamica
    Adicionado barra lateral dinamica nas telas de configurações

--- static/geral/js/sidebar.js
+++ static/geral/js/sidebar.js
@@ -1,17 +1,32 @@
-const GRID_CONTAINER = document.querySelector('.grid-container');
+const MAIN_CONTENT = document.querySelector('.grid-container');
 const SIDEBAR = document.getElementById('sidebar');
-const MAIN_CONTENT = document.getElementById('main');
 const TOGGLER = document.getElementById('sidebar-toggle');
+const TOGGLER_INSIDE = document.getElementById('sidebar-toggle-inside');
+const LINK_TEXT_LIST = document.querySelectorAll('.sidebar-link-name');
+const LINK_LIST = document.querySelectorAll('.sidebar-shortcut');
 let sidebarState = false;
 function ToggleSidebar() {
     sidebarState = !sidebarState;
     if (sidebarState == true) {
-        if (GRID_CONTAINER)
-            GRID_CONTAINER.style.gridTemplateColumns = "300px auto";
+        if (SIDEBAR)
+            SIDEBAR.style.width = "300px";
+        if (MAIN_CONTENT)
+            MAIN_CONTENT.style.marginLeft = "300px";
+        LINK_TEXT_LIST?.forEach((text) => {
+            text.style.opacity = '1';
+            text.style.width = 'auto';
+        });
     }
     else {
-        if (GRID_CONTAINER)
-            GRID_CONTAINER.style.gridTemplateColumns = "56px auto";
+        if (SIDEBAR)
+            SIDEBAR.style.width = "56px";
+        if (MAIN_CONTENT)
+            MAIN_CONTENT.style.marginLeft = "56px";
+        LINK_TEXT_LIST?.forEach((text) => {
+            text.style.opacity = '0';
+            text.style.width = 'auto';
+        });
     }
 }
 TOGGLER?.addEventListener('click', () => { ToggleSidebar(); });
+TOGGLER_INSIDE?.addEventListener('click', () => { ToggleSidebar(); });
